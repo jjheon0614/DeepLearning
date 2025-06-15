8cb7605b664a07aa5fa19a1cc7dc9bf5b5cf59e7
jaeheon <jjheon0614@gmail.com>
2025-06-15 23:36:59 +0900

    update ch01

---
+++ ch01/test_np.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+x = np.array([1.0, 2.0, 3.0])
+print(x)
+type(x)
+
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[3, 0], [0, 6]])
+print(A + B)
+print(A * B)
+
+A = np.array([[1, 2], [3, 4]])
+B = np.array([10, 20])
+print(A * B)
