b2387ddb3dd7b6a38cfcb40f23d2c5c9a24ae781
jaeheon <jjheon0614@gmail.com>
2025-06-28 23:55:44 +0900

    create network

--- matrix.py
+++ matrix.py
@@ -1,9 +1,19 @@
 import numpy as np
 
-A = np.array([[1, 2], [3, 4]])
-print(A.shape)
+# A = np.array([[1, 2], [3, 4]])
+# print(A.shape)
 
-B = np.array([[5, 6], [7, 8]])
-print(B.shape)
+# B = np.array([[5, 6], [7, 8]])
+# print(B.shape)
 
-print(np.dot(A, B))
+# print(np.dot(A, B))
+
+X = np.array([1, 2])
+print(X.shape)
+
+W = np.array([[1, 3, 5], [2, 4, 6]])
+print(W)
+print(W.shape)
+
+Y = np.dot(X, W)
+print(Y)
